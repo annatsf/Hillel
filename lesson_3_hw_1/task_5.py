985794a36c61d7b9a077ce5d7ebc4bbae1396468
Anna Tsatskina <annatsf@yahoo.com>
2022-09-01 10:19:24 +0100

    error correction

--- lesson_3_hw_1/task_5.py
+++ lesson_3_hw_1/task_5.py
@@ -3,7 +3,7 @@
 numbers = (input("Enter a three-digit number: "))
 # конвертирую string в list
 # убираю в списке возможый '-' c помощью метода strip()
-suma = sum([int(i) for i in list(numbers.strip('-'))])
+suma = sum([int(i) for i in numbers.strip('-')])
 print('Sum of digits is', suma)
 
 '''Без використання рядків
@@ -14,8 +14,8 @@ number = abs(int(input("Enter a three-digit number: ")))
 def sum(number):
     sum = 0
     while number > 0:
-# нахожу остаток от деления на 10, использую 
-# целочисленное деление
+    # нахожу остаток от деления на 10, использую 
+    # целочисленное деление
         a = number % 10
         sum = sum + a
         number = number // 10
